4073cd4868fa0f6eecdd986362a2ff888a580aef
aaronmtate <aaronmtate@gmail.com>
2017-06-23 09:37:15 -0400

    Fix terrible speed reporting by pulling everything possible out of the ISR.
    - Also refactor outputs to try to limit expensive calls.

--- ostrich_tach.ino
+++ ostrich_tach.ino
@@ -17,9 +17,11 @@
 LiquidCrystal lcd(12, 11, 6, 5, 4, 2);
 
 // Global Variables
-volatile unsigned long prev_pulse_time = 0; // Timestamp of previous interrupt pulse from sensor.
 volatile unsigned long last_pulse_time = 1; // Timestamp of latest interrupt pulse from sensor.
-volatile byte cycle_count = 2;
+volatile bool stale_display = false;
+
+unsigned long prev_pulse_time = 0; // Timestamp of previous interrupt pulse from sensor.
+byte cycle_count = 2;
 
 void setup() {
   // Set up the LCD's number of columns and rows:
@@ -29,44 +31,53 @@ void setup() {
   lcd.print("diyAudio Ostrich");
 
   pinMode(SENSOR_PIN, INPUT_PULLUP); // Sensor setup
-  attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), calculate_rpm, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), sensor_pulse, FALLING);
 
   Serial.begin(9600);  // Open the serial port at 9600 bps
 }
 
 void loop() {
   // Show waiting message after 5 seconds of no new pulses, but only if pulses were detected.
-  if (micros() - last_pulse_time > 5000000 && cycle_count > 0) {
+  if (stale_display) {
+    calculate_rpm();
+  }
+  else if (micros() - last_pulse_time > 5000000 && cycle_count > 0) {
     lcd.setCursor(0, 1);  // Set cursor at first character, second line.
     lcd.print(" Awaiting Signal");
     cycle_count = 0;
   }
-  delay(1000);
+  // delay(100);
 }
 
-// Sensor pulse completion interrupt callback handler
-void calculate_rpm() {
-  prev_pulse_time = last_pulse_time;
+// Sensor pulse completion interrupt callback handler. Keep this as lightweight as possible.
+void sensor_pulse() {
   last_pulse_time = micros();
+  stale_display = true;
+}
 
-  lcd.setCursor(0, 1);
+void calculate_rpm() {
+  stale_display = false;
 
   if (cycle_count < 2) {
     // Build up a couple of cycles to limit poor reporting.
     cycle_count++;
+    lcd.setCursor(0, 1);
+    lcd.print("  RPM:          "); // Clear the Awaiting Signal message, prep for RPM update
   } else {
     // Avoid DIV0, just in case.
     float rpm = (last_pulse_time > prev_pulse_time) ? 60000000.0/(last_pulse_time - prev_pulse_time) : 0;
-    lcd.print("  RPM: ");
+    lcd.setCursor(7, 1);
     lcd.print(rpm, 4);
-    lcd.print("  ");
 
     // Send update to Phoenix PSU via serial port (D0 and D1) in format XX.XXX[lf][cr]
-    if ((10.0 <= rpm) && (rpm < 100)) {
-      char outstr[6];
+    if ((10.0 <= rpm) && (rpm < 100.0)) {
+      char outstr[8];
       dtostrf(rpm, 6, 3, outstr);
+      outstr[6] = '\n';
+      outstr[7] = '\r';
+
       Serial.print(outstr);
-      Serial.print("\n\r");
     }
   }
+  prev_pulse_time = last_pulse_time;
 }
